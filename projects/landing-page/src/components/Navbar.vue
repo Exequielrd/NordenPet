00abe713dcc1124823ea51f47766e53ca755108d
Exequiel <exequielrd10@gmail.com>
2021-09-09 02:28:53 -0300

    creando el navbar

--- projects/landing-page/src/components/Navbar.vue
+++ projects/landing-page/src/components/Navbar.vue
@@ -3,6 +3,14 @@
     <v-app-bar app prominent flat class="navbar_container">
       <v-app-bar-nav-icon />
       <img src="../assets/logoneg.png" class="navbar_logo" />
+      <div class="search_input_container">
+        <input
+          class="search_input"
+          type="text"
+          placeholder="Buscar prductos..."
+        />
+        <button class="search_button">Buscar</button>
+      </div>
     </v-app-bar>
   </v-app>
 </template>
@@ -28,5 +36,28 @@
   .navbar_logo {
     width: 12rem;
     object-fit: contain;
+  }
+
+  .search_input_container {
+    width: 40rem;
+    background: whitesmoke;
+    border: none;
+    border-radius: 0.2rem;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .search_input_container :focus {
+    outline: none;
+  }
+
+  .search_input {
+    width: 100%;
+    padding: 0.5rem;
+    font-size: 1.1rem;
+  }
+
+  .search_button {
+    background: gray;
   }
 </style>
