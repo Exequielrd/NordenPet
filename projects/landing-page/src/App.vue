a858913747711afc63fce1f8f4fc656ab433c9fe
Exequiel <exequielrd10@gmail.com>
2021-09-09 01:59:34 -0300

    creando el navbar

---
+++ projects/landing-page/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <Navbar />
+  </v-app>
+</template>
+
+<script>
+  import Navbar from './components/Navbar';
+
+  export default {
+    name: 'App',
+
+    components: {
+      Navbar,
+    },
+
+    data: () => ({
+      //
+    }),
+  };
+</script>
---
+++ projects/landing-page/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar app prominent flat class="navbar_container">
+      <v-app-bar-nav-icon />
+      <img src="../assets/logoneg.png" class="navbar_logo" />
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+
+    components: {},
+
+    data: () => ({
+      //
+    }),
+  };
+</script>
+
+<style scoped>
+  .navbar_container {
+    background-image: linear-gradient(#212121, #030303);
+    padding: 1rem 2rem;
+  }
+
+  .navbar_logo {
+    width: 12rem;
+    object-fit: contain;
+  }
+</style>
