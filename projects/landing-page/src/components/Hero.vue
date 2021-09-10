f9ff46f81e0fee4aa06f089d6d06b6c67d31eed3
Exequiel <exequielrd10@gmail.com>
2021-09-10 04:19:27 -0300

    offers

--- projects/landing-page/src/components/Hero.vue
+++ projects/landing-page/src/components/Hero.vue
@@ -1,9 +1,51 @@
 <template>
   <v-main>
-    <v-carousel>
-      <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="slide">
+    <v-carousel cycle hide-delimiters height="auto">
+      <v-carousel-item
+        v-for="(slide, i) in slides"
+        :key="i"
+        :src="slide.src"
+        contain
+      >
       </v-carousel-item>
     </v-carousel>
+    <v-container class="my-10">
+      <div class="offers">
+        <h1 class="headline green--text">OFERTAS NORDENPET</h1>
+        <v-card
+          width="16rem"
+          max-width="374"
+          class="my-15"
+          v-for="index in 6"
+          :key="index"
+        >
+          <v-img
+            height="250"
+            src="https://nordenpet.com.ar/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2019/10/Vitalcan-Balanced-Gato-Control-PH-300x300.jpg.webp"
+            contain
+          ></v-img>
+
+          <v-card-title class="text-center"
+            >Vitalcan Balanced Gato Control PH</v-card-title
+          >
+
+          <v-card-text>
+            <div class="headline text-center">
+              Desde:
+              <span class="font-weight-bold">
+                $1230,00
+              </span>
+            </div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="blue">
+              Comprar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </v-container>
   </v-main>
 </template>
 
@@ -13,12 +55,21 @@
 
     data: () => ({
       slides: [
-        '../assets/slide_1.jpg',
-        '../assets/slide_2.png',
-        '../assets/slide_3.jpg',
-        '../assets/slide_4.jpg',
-        '../assets/slide_5.jpg',
+        { src: require('../assets/slide_1.jpg') },
+        { src: require('../assets/slide_2.png') },
+        { src: require('../assets/slide_3.jpg') },
+        { src: require('../assets/slide_4.jpg') },
+        { src: require('../assets/slide_5.jpg') },
       ],
     }),
   };
 </script>
+
+<style scoped>
+  .offers {
+    max-width: 65rem;
+    width: 100%;
+    background: red;
+    margin: auto;
+  }
+</style>
