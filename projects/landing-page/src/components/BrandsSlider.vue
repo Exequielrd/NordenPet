1c4a067803a45558d43bad30296418e04f063e20
Exequiel <exequielrd10@gmail.com>
2021-09-11 23:49:44 -0300

    slider

---
+++ projects/landing-page/src/components/BrandsSlider.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-sheet class="mx-auto" elevation="8">
+    <v-carousel cycle hide-delimiters height="20rem">
+      <v-slide-group class="pa-4" show-arrows>
+        <v-slide-item v-for="(brand, i) in brands" :key="i">
+          <img :src="brand" alt="" contain class="ma-10" height="200" />
+        </v-slide-item>
+      </v-slide-group>
+    </v-carousel>
+  </v-sheet>
+</template>
+
+<script>
+  export default {
+    name: 'BrandsSlider',
+
+    data: () => ({
+      brands: [
+        'https://nordenpet.com.ar/wp-content/uploads/2021/07/vitalcan-balanced.png',
+        'https://nordenpet.com.ar/wp-content/uploads/2021/07/Vitalcan-complete.png',
+      ],
+    }),
+  };
+</script>
--- projects/landing-page/src/components/Hero.vue
+++ projects/landing-page/src/components/Hero.vue
@@ -9,49 +9,80 @@
       >
       </v-carousel-item>
     </v-carousel>
-    <v-container class="my-10">
-      <div class="offers">
-        <h1 class="headline green--text">OFERTAS NORDENPET</h1>
-        <v-card
-          width="16rem"
-          max-width="374"
-          class="my-15"
-          v-for="index in 6"
-          :key="index"
-        >
+    <v-container class="my-10 pl-0">
+      <div>
+        <div class="title_container">
           <v-img
-            height="250"
-            src="https://nordenpet.com.ar/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2019/10/Vitalcan-Balanced-Gato-Control-PH-300x300.jpg.webp"
+            max-width="2.5rem"
             contain
-          ></v-img>
-
-          <v-card-title class="text-center"
-            >Vitalcan Balanced Gato Control PH</v-card-title
+            src="../assets/footprint.webp"
+            alt=""
+          />
+          <h1 class="offers_title">OFERTAS NORDENPET</h1>
+        </div>
+        <div class="offers">
+          <v-card
+            width="14rem"
+            max-width="374"
+            class="my-15 mx-4 pa-2"
+            v-for="index in 6"
+            :key="index"
           >
+            <v-img
+              height="200"
+              src="https://nordenpet.com.ar/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2019/10/Vitalcan-Balanced-Gato-Control-PH-300x300.jpg.webp"
+              contain
+            ></v-img>
 
-          <v-card-text>
-            <div class="headline text-center">
-              Desde:
-              <span class="font-weight-bold">
-                $1230,00
-              </span>
-            </div>
-          </v-card-text>
+            <v-card-title class="text-center"
+              >Vitalcan Balanced Gato Control PH</v-card-title
+            >
 
-          <v-card-actions>
-            <v-btn color="blue">
-              Comprar
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+            <v-card-text>
+              <div class="headline text-center">
+                Desde:
+                <span class="font-weight-bold">
+                  $1230,00
+                </span>
+              </div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="#f0ad4e" block>
+                Comprar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
       </div>
+    </v-container>
+
+    <v-container class="brands_slider">
+      <section class="brands_header">
+        <v-img
+          width="5rem"
+          class="my-10"
+          contain
+          src="../assets/footprint.webp"
+          alt=""
+        />
+        <h1 class="ma-5 title">LAS MEJORES MARCAS PARA TUS MASCOTAS</h1>
+        <v-btn large color="#85b719" class="white--text my-2">VER TODAS</v-btn>
+      </section>
+      <BrandsSlider />
     </v-container>
   </v-main>
 </template>
 
 <script>
+  import BrandsSlider from './BrandsSlider';
+
   export default {
     name: 'Hero',
+
+    components: {
+      BrandsSlider,
+    },
 
     data: () => ({
       slides: [
@@ -67,9 +98,43 @@
 
 <style scoped>
   .offers {
-    max-width: 65rem;
     width: 100%;
-    background: red;
     margin: auto;
+    display: flex;
+    justify-content: space-between;
+    overflow: scroll;
+  }
+
+  .title_container {
+    display: flex;
+    align-items: center;
+    margin-bottom: 2rem;
+  }
+
+  .offers_title {
+    color: #85b719;
+    font-weight: 400;
+    border-bottom: 2px solid #85b719;
+    width: 100%;
+    margin-left: 1rem;
+  }
+
+  .brands_slider {
+    background: #85b719;
+    position: relative;
+  }
+
+  .brands_header {
+    width: 18rem;
+    padding: 0 2rem;
+    background-image: linear-gradient(#212121, #030303);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    color: whitesmoke;
+    box-shadow: 1rem 0 2rem rgba(26, 26, 26, 0.692);
+    position: absolute;
+    z-index: 5;
   }
 </style>
