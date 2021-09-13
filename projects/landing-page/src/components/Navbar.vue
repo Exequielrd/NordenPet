ab684443ffb51f0f822adb99babe1460a12b9117
Exequiel <exequielrd10@gmail.com>
2021-09-13 15:15:58 -0300

    navbar

--- projects/landing-page/src/components/Navbar.vue
+++ projects/landing-page/src/components/Navbar.vue
@@ -1,18 +1,38 @@
 <template>
-  <v-app-bar app prominent flat class="navbar_container">
-    <img src="../assets/logoneg.png" class="navbar_logo" />
-    <div class="navbar_top_section">
-      <div class="search_input_container">
-        <input
-          class="search_input"
-          type="text"
-          placeholder="Buscar prductos..."
-        />
-        <button class="search_button">Buscar</button>
+  <v-app-bar app class="navbar_container" height="150">
+    <v-img
+      src="../assets/logoneg.png"
+      alt="logo"
+      max-width="12rem"
+      contain
+    ></v-img>
+    <div class="middle_section">
+      <div class="navbar_top_section">
+        <div class="search_input_container">
+          <input
+            class="search_input"
+            type="text"
+            placeholder="Buscar prductos..."
+          />
+          <button class="search_button">Buscar</button>
+        </div>
+        <div>
+          <v-btn large text color="white" class="profile_button"
+            >Mi cuenta</v-btn
+          >
+          <v-btn large outlined color="#ddd">$0,00</v-btn>
+        </div>
       </div>
-      <div>
-        <v-btn large text color="white" class="profile_button">Mi cuenta</v-btn>
-        <v-btn large outlined color="#ddd">$0,00</v-btn>
+      <div class="navbar_bottom_section">
+        <ul>
+          <li>Ofertas</li>
+          <li>Perro</li>
+          <li>Gato</li>
+          <li>Alimentos Humedos</li>
+          <li>Alimentos Terapeuticos</li>
+          <li>Accesorios</li>
+        </ul>
+        <v-btn color="#f0ad4e">Envio Gratis</v-btn>
       </div>
     </div>
   </v-app-bar>
@@ -30,21 +50,27 @@
   .navbar_container {
     background-image: linear-gradient(#212121, #030303);
     padding: 1rem 2rem;
+    height: auto;
   }
 
-  .navbar_logo {
-    width: 15%;
-    object-fit: contain;
+  .middle_section {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
   }
 
   .navbar_top_section {
+    width: 100%;
     display: flex;
     justify-content: space-between;
-    width: 100%;
+    padding-left: 2rem;
   }
 
   .search_input_container {
-    width: 40rem;
+    width: 100%;
+    max-width: 40rem;
     background: whitesmoke;
     border: none;
     border-radius: 0.2rem;
@@ -67,5 +93,23 @@
     color: whitesmoke;
     font-size: 1rem;
     padding: 0.5rem 1rem;
+  }
+
+  .navbar_bottom_section {
+    width: 100%;
+    padding-left: 2rem;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .navbar_bottom_section > ul {
+    width: 80%;
+    list-style: none;
+    padding: 0;
+    display: flex;
+    color: whitesmoke;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 1.2rem;
   }
 </style>
