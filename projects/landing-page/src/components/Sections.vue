c458ffed2055560ac19735025a17881eab6309cf
Exequiel <exequielrd10@gmail.com>
2021-09-13 00:00:24 -0300

    section

--- projects/landing-page/src/components/Sections.vue
+++ projects/landing-page/src/components/Sections.vue
@@ -3,16 +3,16 @@
     <v-container fluid>
       <v-container>
         <v-row>
-          <v-card height="8rem">
-            <v-row>
-              <v-img
-                src="https://nordenpet.com.ar/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/elementor/thumbs/alimento-terapeutico-pab2pfrd0qakung0c1npl03ktm8kiso0lil80cq9s0.png.webp"
-                contain
-              />
-              <v-card-title>
-                ALIMENTOS TERAPEUTICOS
-              </v-card-title>
-            </v-row>
+          <v-card height="8rem" class="d-flex pa-4">
+            <v-img
+              src="https://nordenpet.com.ar/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/elementor/thumbs/alimento-terapeutico-pab2pfrd0qakung0c1npl03ktm8kiso0lil80cq9s0.png.webp"
+              contain
+              width="30%"
+            />
+            <v-card-title class="p">
+              ALIMENTOS TERAPEUTICOS
+              <v-btn>Ver mas</v-btn>
+            </v-card-title>
           </v-card>
         </v-row>
       </v-container>
